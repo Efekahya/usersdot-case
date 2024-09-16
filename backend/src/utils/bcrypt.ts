6db8d5fdcb1fb558aa5b8530b7a5af98ce2b18c3
Efe Kahyaoğlu <efekahya.ek@gmail.com>
2024-09-16 15:04:08 +0300

    refactor: remove unused bcrypt funcs

--- backend/src/utils/bcrypt.ts
+++
@@ -1,9 +0,0 @@
-import bcrypt from "bcrypt";
-
-export const encryptPass = async (password: string) => {
-  return await bcrypt.hash(password, 10);
-};
-
-export const comparePass = async (password: string, hash: string) => {
-  return await bcrypt.compare(password, hash);
-};
